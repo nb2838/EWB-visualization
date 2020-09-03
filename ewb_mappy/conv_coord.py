780ad63dee671878c5e1b031a42dd37925237b74
teegholm <66790083+teegholm@users.noreply.github.com>
2020-09-02 21:44:59 -0400

    Added docstring

--- ewb_mappy/conv_coord.py
+++ ewb_mappy/conv_coord.py
@@ -5,6 +5,14 @@
 
 # -*- coding: utf-8 -*-
 def conv_coord(deg_notation):
+    """
+    Returns the latitude & longitude coordinates, converted from degrees, minutes, & seconds to decimal notation.
+    
+    Parameters
+    ----------
+    deg_notation : string
+        latitude or longitude coordinate string, represented in degrees, minutes, & seconds notation
+    """
     degrees, rest = deg_notation.split('°', 1)
     minutes, rest = rest.split("'", 1)
     seconds, direction = rest.split('"', 1)
